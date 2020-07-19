5afb42aa839d9e6f578f15559b76e8a5fd5de5f9
Nemetz Sergey <kibernetics@gmail.com>
2020-07-19 21:35:55 +0300

    firstcommit

---
+++ src/App.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        
+        final String operands = "+-*/";
+        
+        //String expSample = "IV*  x".trim();
+        // String[] arrNumbers; // = expSample.split("[\\+\\-\\*\\/]");
+
+        Scanner sc = new Scanner(System.in);
+        String expSample;
+
+        System.out.println("Enter expression for calculate:");
+        expSample = sc.nextLine();
+        
+        sc.close();
+
+        try {
+            if (expSample.length() < 3) {
+                throw new Exception("Expression is invalid!");
+            }
+            boolean b = false;
+            for (int i = 1; i < expSample.length()-1; i++) {
+    
+                if (operands.indexOf(expSample.substring(i, i+1)) > -1) {
+                    String[] arrNumbers = new String[3];
+                    arrNumbers[0] = expSample.substring(0, i).trim();;
+                    arrNumbers[1] = expSample.substring(i, i+1);
+                    arrNumbers[2] = expSample.substring(i+1, expSample.length()).trim();
+
+                    NumberSystem fOperandNS = new NumberSystem(arrNumbers[0]);
+                    NumberSystem sOperandNS = new NumberSystem(arrNumbers[2]);
+                    
+                    // проверяем, соответствуют ли системы счисления друг другу
+                    if (fOperandNS.numSystem != sOperandNS.numSystem) {
+                        throw new Exception("Number Systems is not same!");
+                    } else {
+                        final double fValue;
+                        final double sValue;
+                        
+                        // проверяем значение на допустимый диапазон (1 - 10)
+                        // если СС римская, то перводим в арабские числа
+                        if (fOperandNS.numSystem == "R") {
+                            RomanNumeral N = new RomanNumeral(arrNumbers[0]);
+                            fValue = N.toInt();
+                            N = new RomanNumeral(arrNumbers[2]);
+                            sValue = N.toInt();
+                        } else {
+                            fValue = Integer.parseInt(arrNumbers[0]);
+                            sValue = Integer.parseInt(arrNumbers[2]);
+                        }
+        
+                        if (!(fValue > 0 && fValue < 11) && (sValue > 0 && sValue < 11)) {
+                            throw new Exception("Value not in range (1 - 10)");
+                        } else {
+                            double resultInt = 0;
+                            if (arrNumbers[1].equals("+")) {
+                                resultInt = fValue + sValue;
+                            } else if (arrNumbers[1].equals("-")) {
+                                resultInt = fValue - sValue;
+                            } else if (arrNumbers[1].equals("*")) {
+                                resultInt = fValue * sValue;
+                            } else if (arrNumbers[1].equals("/")) {
+                                resultInt = fValue / sValue;
+                            }
+
+                            if (fOperandNS.numSystem == "R") {
+                                if (resultInt < 1) {
+                                    throw new Exception("Result less than 1 can't be present in Roman numbers!");
+                                } else {
+                                    RomanNumeral N = new RomanNumeral((int)(resultInt));
+                                    System.out.println(N.toString());   
+                                }
+                                
+                            } else {
+                                System.out.println(resultInt);
+                            }   
+                        }
+                    }
+
+                    b = true;
+                    break;
+                }
+            }
+            
+            if (!b)
+                throw new Exception("Expression is invalid!");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+    }
+}
---
+++ src/NumberSystem.java
@@ -0,0 +1,35 @@
+public class NumberSystem {
+
+    public final String numSystem; 
+
+    public NumberSystem(String expSample) {
+        final String template = "IVX";
+        String s = "";
+        Boolean b = false;
+        //проверяем выражение на то, что это число
+        for (int i = 0; i < expSample.length(); i++) {
+            if (!Character.isDigit(expSample.charAt(i))) {
+                b = true;
+                break;
+            }
+        }
+
+        if (!b)
+            s = "A";
+        else { //если не число, пытаемся проверить на римскую СС
+            b = false;
+            for (int i = 0; i < expSample.length(); i++) {
+                if (!template.toLowerCase().contains(expSample.substring(i, i+1).toLowerCase())) {
+                    b = true;
+                    break;
+                }
+            }    
+        
+            if (!b)
+                s = "R";
+        }
+
+        numSystem = s;
+    }
+
+}
